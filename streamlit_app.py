d43e1b2825f072fd623219524ad215a96c766297
ladakm1 <128032267+ladakm1@users.noreply.github.com>
2023-03-16 10:10:09 +0100

    Update streamlit_app.py
    added breakfast emoi

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.header ('Breakfast menu')
 streamlit.text ('🥣 Omega 3 & Bluebery Oatmeal')
 streamlit.text ('🥗 Kale, Spinach & Rocekt Smoothy')
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
-stremlit.text ('🥑🍞Avocado Toast')
+stremlit.text ('🥑🍞 Avocado Toast')
 
