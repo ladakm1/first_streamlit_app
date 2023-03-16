23680b3f08fc3fec144dd14c48b799bbeb6eee83
ladakm1 <128032267+ladakm1@users.noreply.github.com>
2023-03-16 10:08:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit
 streamlit.title('My Partens New Healthy Diner')
 streamlit.header ('Breakfast menu')
-streamlit.text (🥣'Omega 3 & Bluebery Oatmeal')
-streamlit.text (🥗'Kale, Spinach & Rocekt Smoothy')
-streamlit.text (🐔 'Hard-Boiled Free-Range Egg')
-stremlit.text (🥑🍞'Avocado Toast')
+streamlit.text ('🥣 Omega 3 & Bluebery Oatmeal')
+streamlit.text ('🥗 Kale, Spinach & Rocekt Smoothy')
+streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
+stremlit.text ('🥑🍞Avocado Toast')
 
